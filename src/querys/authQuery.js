f815277bdb3b433f672bf4cf267f886ead17f86a
JakubWarsinski <wars.jakub@o2.pl>
2025-05-11 19:30:03 +0200

    usiniecie niepotrzebnych folderow

--- src/querys/authQuery.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../../sklep.db');
-const helper = require('../utils/queryHelper');
-
-exports.singInUser = async (email) => {
-    const sql = `SELECT id_uzytkownika, login, email, haslo FROM uzytkownik WHERE email = ?`;
-
-    return await helper.getSingleData(sql, email);
-}
